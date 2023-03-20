175c8839a6f9e8ec2c9a0120df9d00ea118a21ee
alaincormier-ssc <94859304+alaincormier-ssc@users.noreply.github.com>
2023-03-20 14:19:12 -0300

    bash linting setup-kcc.sh line 80-end

--- scripts/bootstrap/setup-kcc.sh
+++ scripts/bootstrap/setup-kcc.sh
@@ -71,43 +71,43 @@ gcloud compute forwarding-rules create endpoint1 \
 gcloud dns managed-zones create googleapis \
 --description="dns zone for googleapis" \
 --dns-name=googleapis.com \
---networks=$NETWORK \
+--networks="$NETWORK" \
 --visibility=private
 
 gcloud dns record-sets create googleapis.com. --zone="googleapis" --type="A" --ttl="300" --rrdatas="10.255.255.254"
 
-gcloud dns record-sets create *.googleapis.com. --zone="googleapis" --type="CNAME" --ttl="300" --rrdatas="googleapis.com."
+gcloud dns record-sets create "*.googleapis.com." --zone="googleapis" --type="CNAME" --ttl="300" --rrdatas="googleapis.com."
 
 # private dns zone for gcr.io
 gcloud dns managed-zones create gcrio \
 --description="dns zone for gcrio" \
 --dns-name=gcr.io \
---networks=$NETWORK \
+--networks="$NETWORK" \
 --visibility=private
 
 gcloud dns record-sets create gcr.io. --zone="gcrio" --type="A" --ttl="300" --rrdatas="10.255.255.254"
 
-gcloud dns record-sets create *.gcr.io. --zone="gcrio" --type="CNAME" --ttl="300" --rrdatas="gcr.io."
+gcloud dns record-sets create "*.gcr.io." --zone="gcrio" --type="CNAME" --ttl="300" --rrdatas="gcr.io."
 
 # Allow egress to AZDO (optional) - should be revised periodically - https://learn.microsoft.com/en-us/azure/devops/organizations/security/allow-list-ip-url?view=azure-devops&tabs=IP-V4#ip-addresses-and-range-restrictions
-gcloud compute firewall-rules create allow-egress-azure --action ALLOW --rules tcp:22,tcp:443 --destination-ranges 13.107.6.0/24,13.107.9.0/24,13.107.42.0/24,13.107.43.0/24 --direction EGRESS --priority 5000 --network $NETWORK --enable-logging
+gcloud compute firewall-rules create allow-egress-azure --action ALLOW --rules tcp:22,tcp:443 --destination-ranges 13.107.6.0/24,13.107.9.0/24,13.107.42.0/24,13.107.43.0/24 --direction EGRESS --priority 5000 --network "$NETWORK" --enable-logging
 
 # Allow egress to Github (optional) - should be revised periodically - https://api.github.com/meta
-gcloud compute firewall-rules create allow-egress-github --action ALLOW --rules tcp:22,tcp:443 --destination-ranges 192.30.252.0/22,185.199.108.0/22,140.82.112.0/20,143.55.64.0/20,20.201.28.151/32,20.205.243.166/32,102.133.202.242/32,20.248.137.48/32,20.207.73.82/32,20.27.177.113/32,20.200.245.247/32,20.233.54.53/32,20.201.28.152/32,20.205.243.160/32,102.133.202.246/32,20.248.137.50/32,20.207.73.83/32,20.27.177.118/32,20.200.245.248/32,20.233.54.52/32 --direction EGRESS --priority 5001 --network $NETWORK --enable-logging
+gcloud compute firewall-rules create allow-egress-github --action ALLOW --rules tcp:22,tcp:443 --destination-ranges 192.30.252.0/22,185.199.108.0/22,140.82.112.0/20,143.55.64.0/20,20.201.28.151/32,20.205.243.166/32,102.133.202.242/32,20.248.137.48/32,20.207.73.82/32,20.27.177.113/32,20.200.245.247/32,20.233.54.53/32,20.201.28.152/32,20.205.243.160/32,102.133.202.246/32,20.248.137.50/32,20.207.73.83/32,20.27.177.118/32,20.200.245.248/32,20.233.54.52/32 --direction EGRESS --priority 5001 --network "$NETWORK" --enable-logging
 
 # Allow egress to internal, peered vpc and secondary ranges
-gcloud compute firewall-rules create allow-egress-internal --action ALLOW --rules=all --destination-ranges 192.168.0.0/16,172.16.0.128/28,10.0.0.0/8 --direction EGRESS --priority 1000 --network $NETWORK --enable-logging
+gcloud compute firewall-rules create allow-egress-internal --action ALLOW --rules=all --destination-ranges 192.168.0.0/16,172.16.0.128/28,10.0.0.0/8 --direction EGRESS --priority 1000 --network "$NETWORK" --enable-logging
 
 # Deny egress to internet
-gcloud compute firewall-rules create deny-egress-internet --action DENY --rules=all --destination-ranges 0.0.0.0/0 --direction EGRESS --priority 65535 --network $NETWORK --enable-logging
+gcloud compute firewall-rules create deny-egress-internet --action DENY --rules=all --destination-ranges 0.0.0.0/0 --direction EGRESS --priority 65535 --network "$NETWORK" --enable-logging
 
 # Create and Config controller
-gcloud anthos config controller create $CLUSTER --location $REGION --network $NETWORK --subnet $SUBNET
+gcloud anthos config controller create "$CLUSTER" --location "$REGION" --network "$NETWORK" --subnet "$SUBNET"
 
 # Config controller get credentials
-gcloud anthos config controller get-credentials $CLUSTER --location $REGION
+gcloud anthos config controller get-credentials "$CLUSTER" --location "$REGION"
 
-export SA_EMAIL="$(kubectl get ConfigConnectorContext -n config-control \
+SA_EMAIL="$(kubectl get ConfigConnectorContext -n config-control \
     -o jsonpath='{.items[0].spec.googleServiceAccount}' 2> /dev/null)"
 
 gcloud organizations add-iam-policy-binding "${ORG_ID}" \
@@ -125,16 +125,16 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
   --role "roles/iam.serviceAccountAdmin" \
   --project "${PROJECT_ID}"
 
-export EMAIL=$(gcloud config list --format json|jq .core.account | sed 's/"//g')
+EMAIL=$(gcloud config list --format json|jq .core.account | sed 's/"//g')
 
 # Adding logging admin role binding
 gcloud organizations add-iam-policy-binding "${ORG_ID}" --member "user:${EMAIL}" --role roles/logging.admin
 
 # Update the logging for region
-gcloud alpha logging settings update --organization=$ORG_ID --storage-location=$REGION
+gcloud alpha logging settings update --organization="$ORG_ID" --storage-location="$REGION"
 
 # Create git-creds for Repo access
-kubectl create secret generic git-creds --namespace="config-management-system" --from-literal=username=${GIT_USERNAME} --from-literal=token=${TOKEN}
+kubectl create secret generic git-creds --namespace="config-management-system" --from-literal=username="${GIT_USERNAME}" --from-literal=token="${TOKEN}"
 
 
 cat << EOF > ./root-sync.yaml
