885d6424a83d7891d27b9c39b3cb05ffe71b81c9
davelanglois-ssc <dave.langlois@ssc-spc.gc.ca>
2023-06-08 16:43:36 +0000

    adding dev-mode to setup-kcc.sh

--- scripts/bootstrap/setup-kcc.sh
+++ scripts/bootstrap/setup-kcc.sh
@@ -13,9 +13,31 @@ SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck source-path=scripts/bootstrap # tell shellcheck where to look
 source "${SCRIPT_ROOT}/../common/print-colors.sh"
 
+# Set a default value for the dev_mode variable
+dev_mode=false
+
+# Use getopts to parse the options
+while getopts ":d" opt; do
+  case ${opt} in
+    d ) # If the -d option is provided, set dev_mode to true
+      dev_mode=true
+      ;;
+    \? ) # If an invalid option is provided, print usage information and exit
+      print_error "Usage: bash setup-kcc.sh [-d] PATH_TO_ENV_FILE
+        -d: dev_mode. It will bootstrap the landing zone in a folder instead than at the org level.
+        It will also deploy an autopilot cluster instead of a standard cluster"
+      exit 1
+      ;;
+  esac
+done
+
+# Shift the positional parameters to remove the options that have been parsed by getopts
+shift $((OPTIND -1))
+
 if [ $# -eq 0 ]; then
-    print_error "No input file provided.
-Usage: bash setup-kcc.sh PATH_TO_ENV_FILE"
+    print_error "Usage: bash setup-kcc.sh [-d] PATH_TO_ENV_FILE
+      -d: dev_mode. It will bootstrap the landing zone in a folder instead than at the org level.
+      It will also deploy an autopilot cluster instead of a standard cluster"
     exit 1
 fi
 
@@ -23,19 +45,32 @@ fi
 # shellcheck disable=SC1090 # don't look for sourced file, it won't exist in this repo
 source "$1"
 
-# Update the logging for region
+print_info "Update the logging for region"
 gcloud alpha logging settings update --organization="$ORG_ID" --storage-location="$REGION"
 
-gcloud resource-manager folders create --display-name="$LZ_FOLDER_NAME" --organization="$ORG_ID" --format="value(name)"
-gcloud projects create "$PROJECT_ID" --set-as-default --organization="$ORG_ID"
+print_info "create folder and project"
+# DEV_MODE: deploy in a folder instead than at the org level
+if [ "$dev_mode" = true ]; then
+  gcloud resource-manager folders create --display-name="$LZ_FOLDER_NAME" --folder="$ROOT_FOLDER_ID" --format="value(name)"
+  gcloud projects create "$PROJECT_ID" --set-as-default --folder="$ROOT_FOLDER_ID"
+else
+  gcloud resource-manager folders create --display-name="$LZ_FOLDER_NAME" --organization="$ORG_ID" --format="value(name)"
+  gcloud projects create "$PROJECT_ID" --set-as-default --organization="$ORG_ID"
+fi
+
+print_info "Link billing account"
 gcloud beta billing projects link "$PROJECT_ID" --billing-account "$BILLING_ID"
+print_info "sleep 30s to allow for project creation before enabling services"
+sleep 30
 gcloud config set project "$PROJECT_ID"
+
+print_info "Enable services"
 gcloud services enable krmapihosting.googleapis.com container.googleapis.com cloudresourcemanager.googleapis.com cloudbilling.googleapis.com serviceusage.googleapis.com servicedirectory.googleapis.com dns.googleapis.com
 
-# VPC
+print_info "VPC"
 gcloud compute networks create "$NETWORK" --subnet-mode=custom
 
-# Subnet
+print_info "Subnet"
 gcloud compute networks subnets create "$SUBNET"  \
 --network "$NETWORK" \
 --range 192.168.0.0/16 \
@@ -44,25 +79,27 @@ gcloud compute networks subnets create "$SUBNET"  \
 --enable-private-ip-google-access \
 --enable-flow-logs --logging-aggregation-interval=interval-5-sec --logging-flow-sampling=1.0 --logging-metadata=include-all
 
-# Cloud router and Cloud NAT
+print_info "Cloud router and Cloud NAT"
 gcloud compute routers create kcc-router --project="$PROJECT_ID"  --network="$NETWORK"  --asn=64513 --region="$REGION"
 gcloud compute routers nats create kcc-router --router=kcc-router --region="$REGION" --auto-allocate-nat-external-ips --nat-all-subnet-ip-ranges --enable-logging
 
-
-# enable logging for dns
+print_info "enable logging for dns"
 gcloud dns policies create dnspolicy1 \
 --networks="$NETWORK" \
 --enable-logging \
 --description="dns policy to enable logging"
 
-# private ip for apis
+print_info "private ip for apis"
 gcloud compute addresses create apis-private-ip \
 --global \
 --purpose=PRIVATE_SERVICE_CONNECT \
 --addresses=10.255.255.254 \
 --network="$NETWORK"
 
-# private endpoint
+print_info "sleep 15s to allow for address to create"
+sleep 15
+
+print_info "private endpoint"
 gcloud compute forwarding-rules create endpoint1 \
 --global \
 --network="$NETWORK" \
@@ -70,7 +107,7 @@ gcloud compute forwarding-rules create endpoint1 \
 --target-google-apis-bundle=all-apis \
 --service-directory-registration=projects/"$PROJECT_ID"/locations/"$REGION"
 
-# private dns zone for googleapis.com
+print_info "private dns zone for googleapis.com"
 gcloud dns managed-zones create googleapis \
 --description="dns zone for googleapis" \
 --dns-name=googleapis.com \
@@ -81,7 +118,7 @@ gcloud dns record-sets create googleapis.com. --zone="googleapis" --type="A" --t
 
 gcloud dns record-sets create "*.googleapis.com." --zone="googleapis" --type="CNAME" --ttl="300" --rrdatas="googleapis.com."
 
-# private dns zone for gcr.io
+print_info "private dns zone for gcr.io"
 gcloud dns managed-zones create gcrio \
 --description="dns zone for gcrio" \
 --dns-name=gcr.io \
@@ -92,22 +129,29 @@ gcloud dns record-sets create gcr.io. --zone="gcrio" --type="A" --ttl="300" --rr
 
 gcloud dns record-sets create "*.gcr.io." --zone="gcrio" --type="CNAME" --ttl="300" --rrdatas="gcr.io."
 
-# Allow egress to AZDO (optional) - should be revised periodically - https://learn.microsoft.com/en-us/azure/devops/organizations/security/allow-list-ip-url?view=azure-devops&tabs=IP-V4#ip-addresses-and-range-restrictions
+print_info "Allow egress to AZDO (optional)"
+# Should be revised periodically - https://learn.microsoft.com/en-us/azure/devops/organizations/security/allow-list-ip-url?view=azure-devops&tabs=IP-V4#ip-addresses-and-range-restrictions
 gcloud compute firewall-rules create allow-egress-azure --action ALLOW --rules tcp:22,tcp:443 --destination-ranges 13.107.6.0/24,13.107.9.0/24,13.107.42.0/24,13.107.43.0/24 --direction EGRESS --priority 5000 --network "$NETWORK" --enable-logging
 
-# Allow egress to Github (optional) - should be revised periodically - https://api.github.com/meta
+print_info "Allow egress to Github (optional)"
+# Should be revised periodically - https://api.github.com/meta
 gcloud compute firewall-rules create allow-egress-github --action ALLOW --rules tcp:22,tcp:443 --destination-ranges 192.30.252.0/22,185.199.108.0/22,140.82.112.0/20,143.55.64.0/20,20.201.28.151/32,20.205.243.166/32,102.133.202.242/32,20.248.137.48/32,20.207.73.82/32,20.27.177.113/32,20.200.245.247/32,20.233.54.53/32,20.201.28.152/32,20.205.243.160/32,102.133.202.246/32,20.248.137.50/32,20.207.73.83/32,20.27.177.118/32,20.200.245.248/32,20.233.54.52/32 --direction EGRESS --priority 5001 --network "$NETWORK" --enable-logging
 
-# Allow egress to internal, peered vpc and secondary ranges
+print_info "Allow egress to internal, peered vpc and secondary ranges"
 gcloud compute firewall-rules create allow-egress-internal --action ALLOW --rules=all --destination-ranges 192.168.0.0/16,172.16.0.128/28,10.0.0.0/8 --direction EGRESS --priority 1000 --network "$NETWORK" --enable-logging
 
-# Deny egress to internet
+print_info "Deny egress to internet"
 gcloud compute firewall-rules create deny-egress-internet --action DENY --rules=all --destination-ranges 0.0.0.0/0 --direction EGRESS --priority 65535 --network "$NETWORK" --enable-logging
 
-# Create and Config controller
-gcloud anthos config controller create "$CLUSTER" --location "$REGION" --network "$NETWORK" --subnet "$SUBNET"
+print_info "Create Config controller"
+# DEV_MODE: Deploy an autopilot cluster instead of a standard cluster
+if [ "$dev_mode" = true ]; then
+  gcloud anthos config controller create "$CLUSTER" --location "$REGION" --network "$NETWORK" --subnet "$SUBNET" --master-ipv4-cidr-block="172.16.0.128/28" --full-management
+else
+  gcloud anthos config controller create "$CLUSTER" --location "$REGION" --network "$NETWORK" --subnet "$SUBNET"
+fi
 
-# Config controller get credentials
+print_info "Config controller get credentials"
 gcloud anthos config controller get-credentials "$CLUSTER" --location "$REGION"
 
 SA_EMAIL="$(kubectl get ConfigConnectorContext -n config-control \
@@ -123,9 +167,8 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
   --role "roles/serviceusage.serviceUsageConsumer" \
   --project "${PROJECT_ID}"
 
-# Create git-creds for Repo access
+print_info "Create git-creds for Repo access"
 kubectl create secret generic git-creds --namespace="config-management-system" --from-literal=username="${GIT_USERNAME}" --from-literal=token="${TOKEN}"
-
 
 cat << EOF > ./root-sync.yaml
 apiVersion: configsync.gke.io/v1beta1
@@ -144,7 +187,9 @@ spec:
     secretRef:
       name: git-creds
 EOF
+
+print_info "Apply root sync"
 kubectl apply -f root-sync.yaml
 
 # Further steps
-print_warning "The root-sync.yaml file should be checked into the <tier1_infra-REPO>"
+print_warning "The root-sync.yaml file should be checked into the <tier1-REPO>"
