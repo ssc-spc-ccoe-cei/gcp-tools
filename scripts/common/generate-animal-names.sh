2734b85df580b2472f1c920d57d62ddcdef8ec6c
lucstjean-ssc <luc.st-jean@ssc-spc.gc.ca>
2024-03-11 21:35:27 +0000

    adding animals and planets scripts/dicts

--- scripts/common/generate-animal-names.sh
+++ scripts/common/generate-animal-names.sh
@@ -5,7 +5,7 @@
 ##############
 #
 # Pass a number as an argument at the end of the command line to generate that number of names
-# Example: To create 10 random names, run 'bash generate-planet-names.sh 10'
+# Example: To create 10 random names, run 'bash generate-animal-names.sh 10'
 #
 ##############
 
@@ -13,25 +13,19 @@
 SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 file_dir="${SCRIPT_ROOT}/../common"
 
-# Check if the argument is provided and numeric
-if [ -z "$1" ] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
-    echo "Usage: $0 <number>"
-    exit 1
-fi
-
 # Declare an indexed array
-declare -a planets
+declare -a animals
 
 # Read the file line by line and populate the array
 while IFS= read -r line; do
-    planets+=("$line")
-done < "$file_dir/planets.txt"
+    animals+=("$line")
+done < "$file_dir/animals.txt"
 
 # produce the number of names requested
 for i in $(seq 1 "$1");
 do
-    # randomly pick a word from the each list
-    random_planets=${planets[ $RANDOM % ${#planets[@]} ]}
+    # randomly pick a word from the list
+    random_animals=${animals[ $RANDOM % ${#animals[@]} ]}
     # echo and set to lowercase
-    echo "$i" - "${random_planets,,}"
+    echo "$i" - "${random_animals,,}"
 done
